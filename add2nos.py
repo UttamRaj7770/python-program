77aa8896b2165c839152dd39cb1e07386f27fd03
UttamRaj7770 <52146682+UttamRaj7770@users.noreply.github.com>
2019-06-24 16:12:16 +0530

    Add files via upload

---
+++ add2nos.py
@@ -0,0 +1,12 @@
+def input():
+    a=int(input("enter a"))
+    b=int(input("enter b"))
+    return a,b
+def add(a,b):
+    return a+b
+def myprint(a,b,mysum):
+    print("%d+%d=%d"%(a,b,mysum))
+def main()
+    a,b=myinput()
+    s=add(a,b)
+    myprint(a,b,s)
