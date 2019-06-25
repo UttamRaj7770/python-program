c8a73dcdc02630a9e8b5cc85862c27a96f5a5a88
UttamRaj7770 <52146682+UttamRaj7770@users.noreply.github.com>
2019-06-25 15:55:58 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,18 @@
+class Basic:
+    def __init__(self,name):
+        self.name=name
+    def show(self):
+            print('Basic--name:%s'%self.name)
+
+obj1=Basic('Apricot')
+obj1.show()
+
+
+
+
+
+
+
+
+
+            
